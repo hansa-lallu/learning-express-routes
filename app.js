4df4a3dda07b44eba554c8defdab10fde7178623
hansa-lallu <hansalallu@gmail.com>
2020-05-04 05:14:49 +0100

    .js file giving an alert for /users page

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 const express = require('express');
 
 const mainRoutes = require('./routes/index');
 
 const app = express();
 
-app.use(mainRoutes);
+app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(mainRoutes)
 app.listen(3000) 
